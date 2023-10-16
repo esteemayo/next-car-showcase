3892ac3f80b24a1811b6636cfee69e9acd1dc934
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 22:39:36 +0100

    fix: add markup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { useMemo } from 'react';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const copyrightLabel = useMemo(() => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return year;
+  }, []);
+
+  return (
+    <footer className='flex flex-col text-blue-100 border-t border-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start items-start gap-6'>
+          <Image
+            src='/svg/logo.svg'
+            width={118}
+            height={18}
+            alt='logo'
+            className='object-contain'
+          />
+          <div className='text-base text-gray-700'>
+            Carbub {copyrightLabel}
+            <br /> All right reserved &copy;
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
