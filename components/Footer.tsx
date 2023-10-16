ac90719f4eaf746d01b2c71402054454df4fe2ce
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 23:09:06 +0100

    fix: add markup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className='flex flex-col text-blue-100 mt-10 border-t border-gray-100'>
+    <footer className='flex flex-col text-blue-100 mt-5 border-t border-gray-100'>
       <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
         <div className='flex flex-col justify-start items-start gap-6'>
           <Image
@@ -44,6 +44,17 @@ const Footer = () => {
               </div>
             );
           })}
+        </div>
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+          <p>@{copyrightLabel} CarHub. All Rights Reserved</p>
+          <div className='footer__copyrights-link'>
+            <Link href='/' className='text-gray-500'>
+              Privacy Policy
+            </Link>
+            <Link href='/' className='text-gray-500'>
+              Terms of Use
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
