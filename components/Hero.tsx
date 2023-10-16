15ad6bdff167698a46f19c04d7f2e45b3fa79bab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 22:03:48 +0100

    fix: add markup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { useCallback } from 'react';
+
 import { Button } from '.';
 
 const Hero = () => {
@@ -23,6 +25,12 @@ const Hero = () => {
           containerStyles='bg-primary-blue text-white rounded-full mt-10'
           onClick={handleScroll}
         />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/img/hero.png' fill alt='' className='object-contain' />
+          <div className='hero__image-overlay' />
+        </div>
       </div>
     </div>
   );
