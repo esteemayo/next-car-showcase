7bffa596c52083b12fc2f047f8aacf16d581037b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 21:20:59 +0100

    fix: add markup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,17 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+          Find, book, or rent a car - quickly and easily!
+        </h1>
+        <p className='hero__subtitle'>
+          Streamline your car rental and experience with our effortless booking
+          process.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
