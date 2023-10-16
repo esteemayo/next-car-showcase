d643c2fad24f3cb38bf3c75c1009fcba0b63b842
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 22:42:58 +0100

    fix: modify

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className='flex flex-col text-blue-100 border-t border-gray-100'>
+    <footer className='flex flex-col text-blue-100 mt-10 border-t border-gray-100'>
       <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
         <div className='flex flex-col justify-start items-start gap-6'>
           <Image
