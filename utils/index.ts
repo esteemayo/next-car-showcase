ff04e13fc0d83b88b03fb4fabf28ca6661956ff3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 20:07:59 +0100

    fix: add generateCarImageUrl function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { CarProps } from '@/types';
 
 export const fetchCars = async () => {
   const headers = {
@@ -29,4 +30,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const API_KEY = 'jrjavascript-mastery';
+
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', API_KEY);
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
