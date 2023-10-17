5ea4bdfc1382800c7dc8bff787ec1abefc0056f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 01:58:58 +0100

    fix: fix an error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
             className='object-contain'
           />
           <p className='text-base text-gray-700'>
-            Carbub {copyrightLabel}
+            Carhub {copyrightLabel}
             <br /> All right reserved &copy;
           </p>
         </div>
@@ -33,28 +33,30 @@ const Footer = () => {
             return (
               <div key={title} className='footer__link'>
                 <h3 className='font-bold'>{title}</h3>
-                {links.map((link) => {
-                  const { title, url } = link;
-                  return (
-                    <Link key={title} href={url} className='text-gray-500'>
-                      {title}
-                    </Link>
-                  );
-                })}
+                <div className='flex flex-col gap-5'>
+                  {links.map((link) => {
+                    const { title, url } = link;
+                    return (
+                      <Link key={title} href={url} className='text-gray-500'>
+                        {title}
+                      </Link>
+                    );
+                  })}
+                </div>
               </div>
             );
           })}
         </div>
-        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-          <p>@{copyrightLabel} CarHub. All Rights Reserved</p>
-          <div className='footer__copyrights-link'>
-            <Link href='/' className='text-gray-500'>
-              Privacy Policy
-            </Link>
-            <Link href='/' className='text-gray-500'>
-              Terms of Use
-            </Link>
-          </div>
+      </div>
+      <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+        <p>@{copyrightLabel} CarHub. All Rights Reserved</p>
+        <div className='footer__copyrights-link'>
+          <Link href='/' className='text-gray-500'>
+            Privacy Policy
+          </Link>
+          <Link href='/' className='text-gray-500'>
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
