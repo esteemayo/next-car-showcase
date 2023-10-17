be07b441b09c71a1a37d0ebd1db378b7e266c8d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 14:36:09 +0100

    fix: add markup

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer: FC<SearchManufacturerProps> = ({
 
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image
@@ -42,36 +42,50 @@ const SearchManufacturer: FC<SearchManufacturerProps> = ({
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setQuery(e.target.value)}
           />
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.map((item) => {
+                return (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                );
+              })}
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
-      <Transition
-        as={Fragment}
-        leave='transition ease-in duration-100'
-        leaveFrom='opacity-100'
-        leaveTo='opacity-0'
-        afterLeave={() => setQuery('')}
-        show={true}
-      >
-        <Combobox>
-          <Combobox.Options>
-            {filteredManufacturers.map((item) => {
-              return (
-                <Combobox.Option
-                  key={item}
-                  className={({ active }) =>
-                    `relative search-manufacturer__option ${
-                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
-                    }`
-                  }
-                  value={item}
-                >
-                  {item}
-                </Combobox.Option>
-              );
-            })}
-          </Combobox.Options>
-        </Combobox>
-      </Transition>
     </div>
   );
 };
