c142981ecb5050218b49e568832f3467a71e4f07
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 02:12:34 +0100

    feat: create SearchBar component

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 import Button from './Button';
 import Hero from './Hero';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
 import Navbar from './Navbar';
 
-export { Button, Footer, Hero, Navbar };
+export { Button, Footer, Hero, Navbar, SearchBar };
