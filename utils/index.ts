a6771ffc2c771eef3b631024fc99e5ef2d591ab2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 14:58:07 +0100

    feat: create index file

---
+++ utils/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const fetchCars = async () => {
+  const headers = {
+    'X-RapidAPI-Key': '6ceafa54a4mshb8e5c3f1600592bp157895jsn05473b1433b6',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+
+  const res = await axios.get(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    {
+      headers,
+    }
+  );
+
+  return res.data;
+};
