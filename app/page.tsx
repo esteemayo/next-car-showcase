80d0c347b1b800d8be17e92cba7165106d0c6cf6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 02:18:25 +0100

    fix: add markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import { Hero } from '@/components';
+import { Filter, Hero, SearchBar } from '@/components';
 
 const Home = () => {
   return (
     <main className='overflow-hidden'>
       <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold capitalize'>Car catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className='home__filters'>
+          <SearchBar />
+          <div className='home__filter-container'>
+            <Filter title='fuel' />
+            <Filter title='year' />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
