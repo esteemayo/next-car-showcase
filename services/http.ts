fbb4f8093cf6e11a3ecbb81cbff4ed8e85c66948
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 15:10:18 +0100

    fix: modify

--- services/http.ts
+++ services/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import logger from './log';
 
 const API = axios.create({
-  baseURL: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+  baseURL: 'https://cars-by-api-ninjas.p.rapidapi.com/v1',
   headers: {
     'X-RapidAPI-Key': '6ceafa54a4mshb8e5c3f1600592bp157895jsn05473b1433b6',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
