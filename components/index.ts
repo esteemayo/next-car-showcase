742d1497e8224f7be3545cdb5bc9d92d1bb05984
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 15:40:26 +0100

    feat: add CarCard component

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,19 @@
 import Button from './Button';
 import Hero from './Hero';
-import Footer from './Footer';
 import SearchBar from './SearchBar';
-import Navbar from './Navbar';
+import Footer from './Footer';
 import SearchManufacturer from './SearchManufacturer';
+import Navbar from './Navbar';
+import CarCard from './CarCard';
 import Filter from './Filter';
 
-export { Button, Filter, Footer, Hero, Navbar, SearchBar, SearchManufacturer };
+export {
+  Button,
+  CarCard,
+  Filter,
+  Footer,
+  Hero,
+  Navbar,
+  SearchBar,
+  SearchManufacturer,
+};
