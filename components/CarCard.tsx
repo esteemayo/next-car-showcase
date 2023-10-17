8b03c3b81b4339e2d3c28e0c8d7bc6056ed1e3d7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 16:19:01 +0100

    fix: add markup

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,36 @@
-import { FC } from 'react';
+'use client';
 
-const CarCard = () => {
-  return <div>CarCard</div>;
+import Image from 'next/image';
+import { FC, useMemo } from 'react';
+
+import { CarProps } from '@/types';
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard: FC<CarCardProps> = ({ car }) => {
+  const { city_mpg, drive, make, model, transmission, year } = car;
+
+  const carRent = useMemo(() => {
+    return calculateCarRent(city_mpg, year);
+  }, []);
+
+  return (
+    <div className='car-card group'>
+      <div className='car-card__contact'>
+        <h2 className='car-card__content-title'>
+          {make} {model}
+        </h2>
+      </div>
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>$</span>
+        {carRent}
+        <span className='self-end text-[14px] font-medium'>/day</span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
