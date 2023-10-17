386b283a49aff9b2f42ba98ad40e3742f945d1f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 02:25:07 +0100

    feat: create SearchManufacturer component

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,7 @@ import Hero from './Hero';
 import Footer from './Footer';
 import SearchBar from './SearchBar';
 import Navbar from './Navbar';
+import SearchManufacturer from './SearchManufacturer';
 import Filter from './Filter';
 
-export { Button, Filter, Footer, Hero, Navbar, SearchBar };
+export { Button, Filter, Footer, Hero, Navbar, SearchBar, SearchManufacturer };
