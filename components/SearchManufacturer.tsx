f4689493d9431e036eb34b4b6036a2c4110a1ef4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-17 02:55:24 +0100

    fix: add markup

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,5 +1,40 @@
-const SearchManufacturer = () => {
-  return <div>SearchManufacturer</div>;
+'use client';
+
+import { FC, useState } from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import Image from 'next/image';
+
+import { SearchManufacturerProps } from '@/types';
+
+const SearchManufacturer: FC<SearchManufacturerProps> = ({
+  manufacturer,
+  setManufacturer,
+}) => {
+  const [query, setQuery] = useState('');
+
+  return (
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+          <Combobox.Button className='absolute top-[14px'>
+            <Image
+              src='/svg/car-logo.svg'
+              width={20}
+              height={20}
+              alt='Car Logo'
+              className='ml-4'
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className='search-manufacturer__input'
+            placeholder='Volkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
