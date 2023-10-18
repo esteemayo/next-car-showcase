c1d410d3a6a3e6e0da21bd9fc08b55d20b226bb5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-18 22:02:29 +0100

    fix: add markup

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useState } from 'react';
+import { FC, Fragment, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { Listbox, Transition } from '@headlessui/react';
@@ -24,6 +24,39 @@ const Filter: FC<CarFilterProps> = ({ title, options }) => {
               className='ml-4 object-contain'
             />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options className='custom-filter__options'>
+              {options.map((option) => {
+                const { title, value } = option;
+                return (
+                  <Listbox.Option
+                    key={title}
+                    value={option}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 px-4 ${
+                        active ? 'bg-primary-blue' : 'text-gray-900'
+                      }`
+                    }
+                  >
+                    {({ selected }) => {
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {title}
+                      </span>;
+                    }}
+                  </Listbox.Option>
+                );
+              })}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
