6e6608e7351ebd4201fcaa5269bdb56898b532d4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-18 02:41:58 +0100

    fix: add markup

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,7 +1,12 @@
-'use client'
+'use client';
 
-const SearchButton = () => {
-  return <div>SearchButton</div>;
-}
+import { FC } from 'react';
+import { SearchButtonProps } from '@/types';
+
+const SearchButton: FC<SearchButtonProps> = ({ otherClasses }) => {
+  return (
+    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}></button>
+  );
+};
 
 export default SearchButton;
