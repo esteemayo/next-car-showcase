2e320994164108bd2e2add4c3a721b3dbde56cdd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-18 21:37:01 +0100

    fix: add markup

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { Listbox, Transition } from '@headlessui/react';
@@ -8,7 +8,26 @@ import { Listbox, Transition } from '@headlessui/react';
 import { CarFilterProps } from '@/types';
 
 const Filter: FC<CarFilterProps> = ({ title, options }) => {
-  return <div>Filter</div>;
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className='w-fit'>
+      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+        <div className='relative w-fit z-10'>
+          <Listbox.Button className='custom-filter__btn'>
+            <span className='block truncate'>{selected.title}</span>
+            <Image
+              src='/svg/chevron-up-down.svg'
+              width={20}
+              height={20}
+              alt='chevron up down'
+              className='ml-4 object-contain'
+            />
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
+  );
 };
 
 export default Filter;
