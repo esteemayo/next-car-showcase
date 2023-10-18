0aa1dc5114589566d1febbf21a162649fc7d6a83
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-18 02:46:12 +0100

    fix: add markup

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import { FC } from 'react';
+import Image from 'next/image';
+
 import { SearchButtonProps } from '@/types';
 
 const SearchButton: FC<SearchButtonProps> = ({ otherClasses }) => {
   return (
-    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}></button>
+    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src='/svg/magnifying-glass.svg'
+        width={40}
+        height={40}
+        alt='magnifying glass'
+        className='object-contain'
+      />
+    </button>
   );
 };
 
