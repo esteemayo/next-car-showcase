e230497ba232bce9757df8c7ed9db723a423522f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-18 02:54:34 +0100

    fix: add model state variable

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import Image from 'next/image';
 import { FormEvent, useCallback, useState } from 'react';
+
 import { SearchButton, SearchManufacturer } from './';
 
 const SearchBar = () => {
+  const [model, setModel] = useState('');
   const [manufacturer, setManufacturer] = useState('');
 
   const handleSearch = useCallback((e: FormEvent<HTMLFormElement>) => {
@@ -18,6 +21,23 @@ const SearchBar = () => {
           setManufacturer={setManufacturer}
         />
         <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='img/model-icon.png'
+          width={25}
+          height={25}
+          alt='car model'
+          className='absolute w-[20px] h-[20px] ml-4'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan'
+          className='searchbar__input'
+        />
       </div>
     </form>
   );
