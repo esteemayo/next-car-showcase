4e170f0d20e606b9e115830eb03a1331bf609d37
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-19 01:45:14 +0100

    fix: fix an error

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ const Home = async ({ searchParams }) => {
               })}
             </div>
             <ShowMore
-              pageNumber={(searchParams.pageNumber || 10) / 10}
+              pageNumber={(searchParams.limit || 10) / 10}
               isNext={(searchParams.limit || 10) > allCars.length}
             />
           </section>
