0ce1427bbf6b0aef6edf31814b963bee899d36b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-19 01:31:59 +0100

    fix: add markup

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,12 +1,30 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 
+import { Button } from './';
 import { ShowMoreProps } from '@/types';
 
 const ShowMore: FC<ShowMoreProps> = ({ pageNumber, isNext }) => {
-  return <div>ShowMore</div>;
+  const router = useRouter();
+
+  const handleNavigation = useCallback(() => {
+    //
+  }, []);
+
+  return (
+    <div className='w-full flex-center gap-5 mt-10'>
+      {!isNext && (
+        <Button
+          title='Show More'
+          btnType='button'
+          containerStyles='bg-primary-blue rounded-full text-white'
+          onClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMore;
