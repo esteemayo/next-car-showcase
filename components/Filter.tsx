7719e28e25bcad8d36d5793c7438eaa45b95b5f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-22 00:45:19 +0100

    fix: fix an error

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -22,7 +22,7 @@ const Filter: FC<CarFilterProps> = ({ title, options, setFilter }) => {
           <Listbox.Button className='custom-filter__btn'>
             <span className='block truncate'>{selected.title}</span>
             <Image
-              src='/chevron-up-down.svg'
+              src='/svg/chevron-up-down.svg'
               width={20}
               height={20}
               className='ml-4 object-contain'
